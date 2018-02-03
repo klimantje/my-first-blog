40e5a09b9557b4878f7806f9b91bfe7b09c16207
An De Rijdt <an.joanna.de.rijdt@ing.nl>
2018-02-03 14:52:20 +0100

    dynamic template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #this filters the blog posts on publush date in the past and orders them by date
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #this gives back the post_list html under blog
+    return render(request, 'blog/post_list.html', {'posts': posts})
